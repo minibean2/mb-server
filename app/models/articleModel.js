06b5293756e2ac1fd618d3f01a029395b18b818a
keithlei01 <keithlei01@gmail.com>
2017-04-04 18:18:15 +0800

    Bugs

--- app/models/articleModel.js
+++ app/models/articleModel.js
@@ -3,13 +3,16 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var articleSchema = new Schema({
-    title : String,
-    preview : String,
-    images : Array,
-    categoryId : String,
-    imageURL : String,
-    body : String,
-    created_date : Date
+    title: String,
+    preview: String,
+    images: Array,
+    category: Object,
+    categoryId: String,
+    imageURL: String,
+    body: String,
+    featured: Boolean,
+    published: Boolean,
+    created_date: Date
 });
 
 var articles = mongoose.model("articles", articleSchema);
