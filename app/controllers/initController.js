e8ace52255872e65fbdf6504085f0925f4631144
keithlei01 <keithlei01@gmail.com>
2017-04-16 17:08:26 +0800

    Ping API

--- app/controllers/initController.js
+++ app/controllers/initController.js
@@ -1,60 +1,74 @@
-var Users = require("../models/userModel");
-var Categories = require("../models/categoryModel");
+var users = require("../models/userModel");
+var categories = require("../models/categoryModel");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-    app.get("/api/initAdminUsers", function(req, res) {
-
-        // seed database
-        var adminUsers = [
-            {
-                login: "admin",
-                password: "admin",
-                lastname: "admin",
-                firstname: "minibean",
-                email: "",
-                mobile: "",
-                active: true,
-                created_date: new Date()
-            }
-        ];
-
-        Users.create(adminUsers, function(err, results) {
-            res.send(results);
-        })
-
+    app.get("/api/ping", function (req, res) {
+        res.status(200).send("pong");
     });
 
-    app.get("/api/initCategories", function(req, res) {
+    app.get("/api/initAdminUsers", function (req, res) {
 
-        // seed database
-        var categories = [
-            {
-                name: "Baby Shower",
-                created_date: new Date()
-            },
-            {
-                name: "Education",
-                created_date: new Date()
-            },
-            {
-                name: "Food",
-                created_date: new Date()
-            },
-            {
-                name: "Sports",
-                created_date: new Date()
-            },
-            {
-                name: "Books",
-                created_date: new Date()
+        users.count(function (err, count) {
+            // empty collection, seed
+            if (!err && count === 0) {
+                var adminUsers = [
+                    {
+                        login: "admin",
+                        password: "admin",
+                        lastname: "admin",
+                        firstname: "minibean",
+                        email: "",
+                        mobile: "",
+                        active: true,
+                        created_date: new Date()
+                    }
+                ];
+
+                users.create(adminUsers, function (err, results) {
+                    res.send(results);
+                })
+            } else {
+                res.send("Users collection not empty.");
             }
-        ];
+        });
+    });
 
-        Categories.create(categories, function(err, results) {
-            res.send(results);
-        })
+    app.get("/api/initCategories", function (req, res) {
 
+        categories.count(function (err, count) {
+            // empty collection, seed
+            if (!err && count === 0) {
+                var cats = [
+                    {
+                        name: "Baby Shower",
+                        created_date: new Date()
+                    },
+                    {
+                        name: "Education",
+                        created_date: new Date()
+                    },
+                    {
+                        name: "Food",
+                        created_date: new Date()
+                    },
+                    {
+                        name: "Sports",
+                        created_date: new Date()
+                    },
+                    {
+                        name: "Books",
+                        created_date: new Date()
+                    }
+                ];
+
+                categories.create(cats, function (err, results) {
+                    res.send(results);
+                })
+            } else {
+                res.send("Categories collection not empty.");
+            }
+        });
     });
 }
 
