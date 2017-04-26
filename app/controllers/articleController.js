f8c0a30388d9990d9f28403fdf3aedf307baa989
keithlei01 <keithlei01@gmail.com>
2017-04-26 19:34:36 +0800

    minor

--- app/controllers/articleController.js
+++ app/controllers/articleController.js
@@ -77,10 +77,10 @@ module.exports = function (app) {
     /*Created By Vinod
         update article by id
     */
-    app.post("/api/update/article/:articleId", function(req, res) {
+    app.post("/api/article/update/:articleId", function (req, res) {
         var articleId = req.params.articleId;
         var article = req.body;
-        articles.update({"_id":articleId}, article,function(err, num) {
+        articles.update({ "_id": articleId }, article, function (err, num) {
             if (err) {
                 res.status(500).send(err);
             }
